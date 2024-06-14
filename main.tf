3b809be82b8cf7b65afda029f23b900cef6a38ab
Cristian Beraha <cristian@cloudops.works>
2024-06-14 18:19:38 -0300

    chore: provider config

--- main.tf
+++ main.tf
@@ -27,12 +27,11 @@ locals {
 }
 
 resource "aws_route53_zone" "this" {
-  for_each = { for k, v in var.zones : k => v }
-
-  name          = lookup(each.value, "domain_name", each.key)
-  comment       = lookup(each.value, "comment", null)
-  force_destroy = lookup(each.value, "force_destroy", false)
-
+  provider          = aws.default
+  for_each          = { for k, v in var.zones : k => v }
+  name              = lookup(each.value, "domain_name", each.key)
+  comment           = lookup(each.value, "comment", null)
+  force_destroy     = lookup(each.value, "force_destroy", false)
   delegation_set_id = lookup(each.value, "delegation_set_id", null)
 
   #   dynamic "vpc" {
